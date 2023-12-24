0a8cb412e19ce94eb25835c1a850b1abe1d88609
Roman_Batrakov <kyct69@yandex.ru>
2023-12-24 16:12:38 +0300

    Made HomeWork for first lesson

--- Lesson_01/src/main/java/App.java
+++ Lesson_01/src/main/java/App.java
@@ -1,3 +1,5 @@
+import Domain.Bottle;
+import Domain.HotDrink;
 import Domain.Product;
 import Services.CoinDispenser;
 import Services.Display;
@@ -6,6 +8,7 @@ import Services.VendingMachine;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
@@ -13,8 +16,16 @@ public class App {
         Product item_2 = new Product(120, 2, "Nuts", 2);
         Product item_3 = new Product(90, 3, "Chips", 3);
         Product item_4 = new Product(30, 4, "Bread", 4);
-        Product item_5 = new Product(60, 5, "Coffee", 5);
+        Product item_5 = new Product(60, 5, "Cheese", 5);
         Product item_6 = new Product(150, 6, "Breakers", 6);
+        Product item_7 = new Bottle(80, 7, "Cola", 7, 0.5f);
+        //Домашнее задание по добавлению напитков
+        Product item_8 = new HotDrink(110, 8, "Tea", 8, 0.2f, 75);
+        Product item_9 = new HotDrink(130, 9, "Tea", 9, 0.3f, 75);
+        Product item_10 = new HotDrink(180, 10, "Tea", 10, 0.5f, 75);
+        Product item_11 = new HotDrink(145, 11, "Coffee", 11, 0.2f, 85);
+        Product item_12 = new HotDrink(180, 12, "Coffee", 12, 0.3f, 85);
+        Product item_13 = new HotDrink(220, 13, "Coffee", 13, 0.5f, 85);
 
         Holder hold = new Holder();
         CoinDispenser coin = new CoinDispenser();
@@ -27,6 +38,16 @@ public class App {
         productList.add(item_4);
         productList.add(item_5);
         productList.add(item_6);
+        productList.add(item_7);
+        
+        //Добавляем вывод новых продуктов
+        productList.add(item_8);
+        productList.add(item_9);
+        productList.add(item_10);
+        productList.add(item_11);
+        productList.add(item_12);
+        productList.add(item_13);
+
 
         VendingMachine vm = new VendingMachine(hold, coin, display, productList);
 
